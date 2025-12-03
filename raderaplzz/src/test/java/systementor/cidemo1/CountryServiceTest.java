1381f1eeecfc3e6ace37341c1256c5fb7f91e4e2
Rabiya Dar <rabiya_khalid@hotmail.com>
2025-12-03 14:45:00 +0100

    add dependencies in pom fil, add Testfile i.e. coutryServiceTest maven file change the testname from calculator test to Countryservicetest

---
+++ raderaplzz/src/test/java/systementor/cidemo1/CountryServiceTest.java
@@ -0,0 +1,48 @@
+package systementor.cidemo1;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+public class CountryServiceTest {
+    @Mock
+    CountryApiClient countryApiClient;
+    @InjectMocks
+    CountryService countryService;
+    private final static Logger logger = Logger.getLogger(CountryServiceTest.class.getName());
+    private static final Country LATVIA = new Country(
+"Latvia",
+        "Republic of Latvia",
+        "Latvija",
+        "Latvijas Republikas",
+        "Riga",
+        "Europe",
+        "Northern Europe",
+        Map.of(
+        "lav", "Latvian",
+        "eng", "English"
+        ),
+         List.of("EST","LTU", "RUS", "BLR"),
+        1829000,
+        64559.0,
+            "https://goo.gl/maps/iQpUkH7ghq31ZtXe9"
+    );
+    @Test
+    void getSortedLanguagesReturnAlphabeticallySortedLanguages(){
+        logger.info("Starting test GetsortedLanguages returns alphabeticallysorted languages");
+        when (countryApiClient.fetchCountryByName("Latvia"))
+                .thenReturn(LATVIA);
+
+        var result = countryService.getSortedLanguages("Latvia");
+        logger.info("Result from get sorted langugaes return " + result);
+
+        assertEquals(List.of("English", "Latvian"), result);
+    }
+}
